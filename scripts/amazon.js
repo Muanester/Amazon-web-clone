716ed62ae29531c2d428fd0c13c5742affec430e
Muanester <stevenmutisya999@gmail.com>
2023-07-10 22:51:34 +0300

    Select Options

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -1,4 +1,5 @@
-// cartQuantityCount();
+
+cartQuantityCount();
 
 let productsHTML = '';
 
@@ -57,47 +58,26 @@ products.forEach(product => {
 
 document.querySelector('.js-products-grid').innerHTML = productsHTML;
 
-// let addedToCart = false;
-
-// document.querySelectorAll('.js-add-to-cart').forEach((button, index)=> {
-//   button.addEventListener('click', ()=> {
-//     addedToCart = true;
-//     if (addedToCart) {
-//       const addedDisp = document.querySelectorAll('.js-added-to-cart');
-//       addedThis = addedDisp[index];
-//       addedThis.style.opacity = 1;
-//       setTimeout(()=> {
-//         addedThis.style.opacity = 0;
-//       }, 1000);
-//     }
-    
-//     handleSelectedValue(value);
-//   });
-// });
-
-// function cartQuantityCount() {
-//   let cartQuantity = 0;
-//   cart.forEach((item)=> {
-//     cartQuantity+=item.quantity;
-//   });
-//   document.querySelector('.js-cart-quantity').innerHTML = cartQuantity;
-// }
-
-
-// selectValue();
+let thisCallBack = false;
+let resetValue = true;
 function selectValue(callback) {
   document.querySelectorAll('.js-product-quantity-container select').forEach((selectOption)=> {
     selectOption.addEventListener('change', ()=> {
+      resetValue = false;
+      thisCallBack = true;
       const selectedOption = selectOption.options[selectOption.selectedIndex];
       const selectedValue = selectedOption.value;
-      if (typeof callback === 'function') {
+      if (typeof callback === 'function' && !resetValue) {
         callback(selectedValue);
+        resetValue = true;
+        thisCallBack = false;
       }
-      
     });
+    if (!thisCallBack && resetValue) {
+      callback(1);
+    }
   });
 }
-
 
 
 function handleSelectedValue(value) {
@@ -113,31 +93,40 @@ function handleSelectedValue(value) {
         setTimeout(()=> {
           addedThis.style.opacity = 0;
         }, 1000);
-
-        let cartQuantity = optVal;
-        // cartQuantity+=item.quantity;
-        document.querySelector('.js-cart-quantity').innerHTML = cartQuantity;
-  
-        const productId = button.dataset.productId;
-        cart.push({
-          productId: productId,
-          quantity: optVal
-        });
-        console.log(cart);
       }
 
-     
+      const productId = button.dataset.productId;
+      let matchingItem;
+      cart.forEach((item)=> {
+        if (productId === item.productId) {
+          matchingItem = item;
+        } 
+      });
+  
+      if (matchingItem) {
+        return;
+      } else {
+          cart.push({
+            productId: productId,
+            quantity: optVal
+          });        
+      }
+
+      console.log(cart);
       localStorage.setItem('cartItems', JSON.stringify(cart));
       
-      // cart.push({
-      //   productId: productId,
-      //   quantity: optVal
-      // });
-      // console.log(cart);
-      // localStorage.setItem('cartItems', JSON.stringify(cart));
       
+      cartQuantityCount();
     })  
 })
 }
 
 selectValue(handleSelectedValue);
+
+function cartQuantityCount() {
+  let cartQuantity = 0;
+  cart.forEach((item)=> {
+    cartQuantity+=item.quantity;
+  });
+  document.querySelector('.js-cart-quantity').innerHTML = cartQuantity;
+}
