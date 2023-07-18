10d39a6776afe5b96bb40558328bccaee2f63c1c
Muanester <stevenmutisya999@gmail.com>
2023-07-18 12:50:22 +0300

    Complete Interactive

--- scripts/orders.js
+++ scripts/orders.js
@@ -13,6 +13,8 @@ function renderOrderItems() {
   let estimateTax = 0;
   let orderTotal = 0;
   let cartQuantity = 0;
+  let currentDate = Date();
+  const orderId = new Date().getTime();
   orders.forEach((item)=> {
     cartQuantity+=item.quantity;
     products.forEach((product)=> {
@@ -62,33 +64,33 @@ function renderOrderItems() {
         }
       });
     });
-  
-    orderObjectsHTML += `
-      <div class="order-header">
-        <div class="order-header-left-section">
-          <div class="order-date">
-            <div class="order-header-label">Order Placed:</div>
-            <div>August 12</div>
-          </div>
-          <div class="order-total">
-            <div class="order-header-label">Total:</div>
-            <div>$${(orderTotal/100).toFixed(2)}</div>
-          </div>
-        </div>
+  }
 
-        <div class="order-header-right-section">
-          <div class="order-header-label">Order ID:</div>
-          <div>27cba69d-4c3d-4098-b42d-ac7fa62b7664</div>
+  orderObjectsHTML += `
+    <div class="order-header">
+      <div class="order-header-left-section">
+        <div class="order-date">
+          <div class="order-header-label">Order Placed:</div>
+          <div>${currentDate}</div>
+        </div>
+        <div class="order-total">
+          <div class="order-header-label">Total:</div>
+          <div>$${(orderTotal/100).toFixed(2)}</div>
         </div>
       </div>
 
-      <div class="order-details-grid js-order-details-grid"></div>
-      `;
+      <div class="order-header-right-section">
+        <div class="order-header-label">Order Id:</div>
+        <div>${orderId}</div>
+      </div>
+    </div>
+
+    <div class="order-details-grid js-order-details-grid"></div>
+  `;
   
-    orderContainerHTML += `
-      <div class="order-container js-order-container"></div>
-    `;
-  }
+  orderContainerHTML = `
+    <div class="order-container js-order-container"></div>
+  `;
   
   document.querySelector('.js-orders-grid').innerHTML = orderContainerHTML;
   document.querySelector('.js-order-container').innerHTML = orderObjectsHTML;
@@ -140,5 +142,3 @@ function createOrder(selectedItems) {
   ordersArray.push(order);
   return order;
 }
-
-
